4e986f0b2979cac794680408fe5185f1b1cd5268
Paksawat Rakthai <paksawat.rakthai@mail.kmutt.ac.th>
2022-05-05 16:21:55 +0700

    install axios and jwt decode

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Landingpage from '../views/Landingpage.vue'
+import Register from '../views/Register.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/',
     name: 'Landingpage',
     component: Landingpage
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   }
  
 ]
