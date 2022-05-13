f15c08a04c20dd964ab6e47fd67164c5ba421dd5
Paksawat Rakthai <paksawat.rakthai@mail.kmutt.ac.th>
2022-05-13 16:37:47 +0700

    install vuex

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+import axios from 'axios'
+
+
+export default createStore({
+    state:{
+
+    },
+    mutations:{
+        
+    },
+    actions:{
+
+    },
+    modules: {
+
+    }
+})
