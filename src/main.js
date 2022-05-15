88197322a0b6e5fe1633e55bb0cc6b3e93f46b8c
Paksawat Rakthai <paksawat.rakthai@mail.kmutt.ac.th>
2022-05-15 21:23:16 +0700

    add dependencies

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import router from './router'
 import './assets/tailwind.css'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
-const app = createApp(App)
+import store from './store'
+
+ 
+const app = createApp(App).use(store)
 app.component('nav-bar',Navbar)
 app.component('foo-ter',Footer)
+
 app.use(router).mount('#app')
+
+
